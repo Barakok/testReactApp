ecb9b504a168bf13380aa53d4cbd3a8f634cbd45
miracletau <alliot19@list.ru>
2023-07-17 01:51:31 +0300

    1

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -2,6 +2,31 @@ import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
+export async function POST(request) {
+  const r = await request.json()
+  try {
+    const { gender, height, weight, age, lifestyle, goal } = r;
+    console.log("ошибка", r)
+    const newUser = await prisma.user.create({
+      data: {
+        gender,
+        height,
+        weight,
+        age,
+        goal,
+        lifestyleCoefficient: {
+          connect: { type: lifestyle },
+        },
+      },
+    });
+    console.log('newUser', newUser);
+    return NextResponse.json(newUser);
+  } catch (error) {
+    console.error('Ошибка при сохранении пользователя:', error);
+    return NextResponse.json({ error: 'Ошибка при сохранении пользователя.' }, { status: 500 });
+  }
+}
+
 export async function GET() {
   try {
     const users = await prisma.user.findMany();
@@ -11,3 +36,4 @@ export async function GET() {
   }
   return NextResponse.json({ message: 'get users' });
 }
+
